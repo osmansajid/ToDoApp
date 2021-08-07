b7b1fa56add5b92f7311dde5c503e84eead391c2
Sajid <osmansazid13@gmail.com>
2021-08-07 21:38:30 +0600

    Solving circular dependency in hilt and database

--- app/src/main/java/com/example/todoapp/data/TaskItemDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/TaskItemDatabase.kt
@@ -2,11 +2,33 @@ package com.example.todoapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.todoapp.data.TaskItem
 import com.example.todoapp.data.TaskItemDao
+import com.example.todoapp.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(entities = [TaskItem::class],version = 1)
 abstract class TaskItemDatabase: RoomDatabase() {
 
     abstract fun taskItemDao() : TaskItemDao
+
+    class Callback @Inject constructor(@ApplicationScope private val coroutineScope: CoroutineScope,
+                                       private val taskItemDatabase: Provider<TaskItemDatabase>
+                                       ): RoomDatabase.Callback(){
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = taskItemDatabase.get().taskItemDao()
+            coroutineScope.launch {
+                dao.insert(TaskItem("Read books"))
+                dao.insert(TaskItem("Go for walk"))
+                dao.insert(TaskItem("Complete assignments"))
+                dao.insert(TaskItem("Complete project work",isImportant = true))
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/todoapp/di/AppModule.kt
+++ app/src/main/java/com/example/todoapp/di/AppModule.kt
@@ -2,12 +2,16 @@ package com.example.todoapp.di
 
 import android.app.Application
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.todoapp.data.TaskItemDao
 import com.example.todoapp.data.TaskItemDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -16,11 +20,22 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideTaskItemDb(app: Application): TaskItemDatabase = Room.databaseBuilder(app,
+    fun provideTaskItemDb(app: Application,callback: RoomDatabase.Callback): TaskItemDatabase = Room.databaseBuilder(app,
         TaskItemDatabase::class.java,"TASK_DATABASE")
         .fallbackToDestructiveMigration()
+        .addCallback(callback)
         .build()
 
     @Provides
     fun provideTaskItemDao(taskDb : TaskItemDatabase): TaskItemDao = taskDb.taskItemDao()
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScopeCoroutine() = CoroutineScope(SupervisorJob())
 }
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationScope
